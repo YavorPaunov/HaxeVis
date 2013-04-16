8691d4f9fa2f3d5b1203a91a247b204264f46373
yavorpaunov <kornerbg@gmail.com>
2013-04-16 20:40:42 +0200

    project created

---
+++ src/Main.hx
@@ -0,0 +1,39 @@
+package ;
+
+import nme.display.Sprite;
+import nme.events.Event;
+import nme.Lib;
+
+/**
+ * ...
+ * @author Yavor
+ */
+
+class Main extends Sprite 
+{
+	
+	public function new() 
+	{
+		super();
+		#if iphone
+		Lib.current.stage.addEventListener(Event.RESIZE, init);
+		#else
+		addEventListener(Event.ADDED_TO_STAGE, init);
+		#end
+	}
+
+	private function init(e) 
+	{
+		// entry point
+	}
+	
+	static public function main() 
+	{
+		var stage = Lib.current.stage;
+		stage.scaleMode = nme.display.StageScaleMode.NO_SCALE;
+		stage.align = nme.display.StageAlign.TOP_LEFT;
+		
+		Lib.current.addChild(new Main());
+	}
+	
+}
