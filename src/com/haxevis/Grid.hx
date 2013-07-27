3908fbcec76455e28c4f2db04f3590c6fd1881e5
yavorpaunov <kornerbg@gmail.com>
2013-07-28 01:45:36 +0200

    Added graphical customization options. Added axis ticks auto generation.

--- src/com/haxevis/Grid.hx
+++ src/com/haxevis/Grid.hx
@@ -6,11 +6,24 @@ import flash.geom.Point;
 import flash.text.TextField;
 import flash.text.TextFieldAutoSize;
 
+using com.haxevis.DataSetItem;
 /**
  * ...
  * @author Yavor
  */
 
+typedef GridGraphicsElement = {
+	thickness:Float,
+	alpha:Float,
+	color:Int
+}
+typedef GridGraphics = {
+	ticks: GridGraphicsElement,
+	gridlines: GridGraphicsElement,
+	axislines: GridGraphicsElement,
+	borders: GridGraphicsElement
+}
+	
 class Grid extends Chart {
 	
 	// Gap X and Y gaps between the edge of the sprite and the beginning of the chart. That is where the tick labels will be.
@@ -21,7 +34,47 @@ class Grid extends Chart {
 	private static inline var HEIGHT:Float = 300;
 	private static inline var WIDTH:Float = 300;
 	
+	public var gridGraphics:GridGraphics;
 	
+	@:isVar public var ticksX(default, set):Array<Float>;
+	private function set_ticksX(value:Array<Float>):Array<Float> {
+		if (autoRedraw) {
+			draw();			
+		}
+		return this.ticksX = value;
+	}
+	
+	@:isVar public var ticksY(default, set):Array<Float>;
+	private function set_ticksY(value:Array<Float>):Array<Float> {
+		if (autoRedraw) {
+			draw();
+		}
+		return this.ticksY = value;
+	}
+	
+	@:isVar public var autoTicks(default, set):Bool;
+	function set_autoTicks(value:Bool):Bool {
+		if (value) {
+			calculateTicks();
+		}
+		return this.autoTicks = value;
+	}
+	
+	@:isVar public var lineX(default, set):Float;
+	function set_lineX(value:Float):Float {
+		if (autoRedraw) {
+			draw();
+		}
+		return this.lineX = value;
+	}
+	
+	@:isVar public var lineY(default, set):Float;
+	function set_lineY(value:Float):Float {
+		if (autoRedraw) {
+			draw();
+		}
+		return this.lineY = value;
+	}
 	
 	@:isVar public var xTop(get, set):Float;
 	private function get_xTop():Float {
@@ -30,6 +83,9 @@ class Grid extends Chart {
 	
 	private function set_xTop(value:Float):Float {
 		this.xTop = value;
+		if (this.xTop < this.xBottom) {
+			throw new Error("Top x cannot be lower than bottom x.");
+		}
 		if(autoRedraw) {
 			draw();
 		}
@@ -38,6 +94,9 @@ class Grid extends Chart {
 	
 	@:isVar public var xBottom(get, set):Float;
 	function get_xBottom():Float {
+		if (this.xTop < this.xBottom) {
+			throw new Error("Top x cannot be lower than bottom x.");
+		}
 		return this.xBottom;
 	}
 	
@@ -48,20 +107,7 @@ class Grid extends Chart {
 		}
 		return this.xBottom;
 	}
-	
-	@:isVar public var xDel(get, set):Float;
-	function get_xDel():Float {
-		return this.xDel;
-	}
-	
-	function set_xDel(value:Float):Float {
-		this.xDel = value;
-		if(autoRedraw) {
-			draw();
-		}
-		return this.xDel;
-	}
-	
+		
 	@:isVar public var yTop(get, set):Float;
 
 	private function get_yTop():Float {
@@ -70,12 +116,14 @@ class Grid extends Chart {
 	
 	private function set_yTop(value:Float):Float {
 		this.yTop = value;
+		if (this.yTop < this.yBottom) {
+			throw new Error("Top y cannot be lower than bottom y.");			
+		}
 		if(autoRedraw) {
 			draw();
 		}
 		return this.yTop;
 	}
-	
 	
 	@:isVar public var yBottom(get, set):Float;
 
@@ -85,42 +133,15 @@ class Grid extends Chart {
 	
 	function set_yBottom(value:Float):Float {
 		this.yBottom = value;
+		if (this.yTop < this.yBottom) {
+			throw new Error("Top y cannot be lower than bottom y.");			
+		}
 		if(autoRedraw) {
 			draw();
 		}
 		return this.yBottom;
-	}
-	
-	
-	@:isVar public var yDel(get_yDel, set_yDel):Float;
-
-	function get_yDel():Float {
-		return this.yDel;
-	}
-	
-	function set_yDel(value:Float):Float {
-		this.yDel = value;
-		if(autoRedraw) {
-			draw();
-		}
-		return this.yDel;
-	}
-	
-	
-	@:isVar public var lineAtZero(get, set):Bool;
-	
-	function get_lineAtZero():Bool {
-		return this.lineAtZero;
-	}
-	
-	function set_lineAtZero(value:Bool):Bool {
-		this.lineAtZero = value;
-		if(autoRedraw) {
-			draw();
-		}
-		return this.lineAtZero;
-	}
-	
+	}	
+		
 	private var ratio:Point;
 	@:isVar public var showGridY(default, set):Bool;
 	function set_showGridY(value:Bool):Bool {
@@ -145,37 +166,55 @@ class Grid extends Chart {
 
 	public var autoRedraw:Bool;
 	
-	public function new(xTop:Float, xBottom:Float, xDel:Float, yTop:Float, yBottom:Float, yDel:Float) {
+	public function new() {
 		super();
 		
 		cacheAsBitmap = true;
+		this.autoTicks = false;
 		
-		this.lineAtZero = false;
 		this.showGridX = false;
 		this.showGridY = false;
 		
-		this.xTop = xTop;
-		this.xBottom = xBottom;
-		this.xDel = xDel;
+		this.lineX = 0;
+		this.lineY = 0;
 		
-		this.yTop = yTop;
-		this.yBottom = yBottom;
-		this.yDel = yDel;
+		this.ticksX = [];
+		this.ticksY = [];
 		
-		if (this.xTop < this.xBottom) {
-			throw new Error("Top x cannot be lower than bottom x.");
+		this.gridGraphics = {
+			ticks: {
+				thickness:1,
+				alpha:1,
+				color:0xAAAAAA
+			},
+			
+			gridlines: {
+				thickness:1,
+				alpha:1,
+				color:0xDEDEDE
+			},
+			
+			axislines: {
+				thickness:1,
+				alpha:1,
+				color:0x121212
+			},
+			
+			borders: {
+				thickness:1,
+				alpha:1,
+				color:0
+			}
 		}
 		
-		if (this.yTop < this.yBottom) {
-			throw new Error("Top y cannot be lower than bottom y.");			
-		}
-		
-		draw();
 	}
 	
 	override private function draw():Void {
-		
 		graphics.clear();
+		
+		
+		
+		
 		while (numChildren > 0) {
 			removeChildAt(0);
 		}
@@ -202,113 +241,93 @@ class Grid extends Chart {
 			yZeroPos = Y+HEIGHT;
 		}
 
-
-		//graphics.beginFill(0, 1);
-		var xLowestDel:Float = this.xBottom - this.xBottom % this.xDel;
-		var xHighestDel:Float = this.xTop - this.xTop % this.xDel;
-		var xCurDel:Float = xLowestDel;
-		while (xCurDel <= xHighestDel) {
-			// xCurDel or (xCurDel - xLowestDel)
-
-			var targetX:Float = Math.min(xCurDel, xCurDel - xLowestDel) * xRatio +xZeroPos; 
+		for (tickX in this.ticksX) {
+			
+			var targetX:Float = tickX * this.ratio.x + xZeroPos; 
 			var targetY:Float;
+			targetY = Y + HEIGHT; //yZeroPos - lineY * this.ratio.y;
+			
+			// Add ticks			
+			graphics.lineStyle(gridGraphics.ticks.thickness, gridGraphics.ticks.color, gridGraphics.ticks.alpha);
+			graphics.moveTo(targetX, targetY);
+			graphics.lineTo(targetX, targetY+4);
+						
+			if (this.showGridX) {
+				graphics.lineStyle(gridGraphics.gridlines.thickness, gridGraphics.gridlines.color, gridGraphics.gridlines.alpha);
+				graphics.moveTo(targetX, Y);
+				graphics.lineTo(targetX, Y + HEIGHT);
+			}
+			graphics.lineStyle();
+			
+			var valueStringRaw:String = Std.string(tickX);
+			var floatingPointIndex:Int = valueStringRaw.indexOf(".");
+			var valueString:String;
 
-			if (this.lineAtZero) {
-				targetY = yZeroPos;
+			if(floatingPointIndex >= 0){
+				valueString = valueStringRaw.substr(0, floatingPointIndex + 3);
 			} else {
-				targetY = Y + HEIGHT;
+				valueString = valueStringRaw;
 			}
-
-			graphics.beginFill(0, 1);
-			graphics.drawRect(targetX - 0.5, targetY, 1, 4);
-			graphics.endFill();
-
-			if(this.showGridX) {
-				graphics.beginFill(0, 0.1);
-				graphics.drawRect(targetX - 0.5, Y, 1, HEIGHT);
-				graphics.endFill();
-			}
-
-			if(xCurDel != 0 || !this.lineAtZero){
-				var valueStringRaw:String = Std.string(xCurDel);
-				var floatingPointIndex:Int = valueStringRaw.indexOf(".");
-				var valueString:String;
-
-				if(floatingPointIndex >= 0){
-					valueString = valueStringRaw.substr(0, floatingPointIndex + 3);
-				} else {
-					valueString = valueStringRaw;
-				}
-
-				var valueField:TextField = new TextField();
-				valueField.selectable = false;
-				valueField.text = valueString;
-				valueField.autoSize = TextFieldAutoSize.LEFT;
-				valueField.x = targetX - valueField.width / 2;
-				valueField.y = targetY + 2;
-				addChild(valueField);
-			}
-			xCurDel += this.xDel;
-
+			var valueField:TextField = new TextField();
+			valueField.selectable = false;
+			valueField.text = valueString;
+			valueField.autoSize = TextFieldAutoSize.LEFT;
+			valueField.x = targetX - valueField.width / 2;
+			valueField.y = targetY + 2;
+			addChild(valueField);
 		}
-
-		var yLowestDel:Float = this.yBottom - this.yBottom % this.yDel;
-		var yHighestDel:Float =this.yTop - this.yTop % this.yDel;
-		var yCurDel:Float = yLowestDel;
-		while (yCurDel <= yHighestDel) {
+		
+		for (tickY in this.ticksY) {
 			var targetX:Float;
-			if (this.lineAtZero) {
-				targetX = xZeroPos;
+			
+			targetX = X; //xZeroPos + lineX * this.ratio.x;
+			
+			// Add ticks
+			var targetY:Float = yZeroPos - tickY * this.ratio.y;
+			
+			graphics.lineStyle(gridGraphics.ticks.thickness, gridGraphics.ticks.color, gridGraphics.ticks.alpha);
+			graphics.moveTo(targetX, targetY);
+			graphics.lineTo(targetX-4, targetY);
+			graphics.lineStyle();
+			
+			if (this.showGridY) {
+				graphics.lineStyle(gridGraphics.gridlines.thickness, gridGraphics.gridlines.color, gridGraphics.gridlines.alpha);
+				graphics.moveTo(X, targetY);
+				graphics.lineTo(X + WIDTH, targetY);				
+			}
+			
+			var valueStringRaw:String = Std.string(tickY);
+			var floatingPointIndex:Int = valueStringRaw.indexOf(".");
+			var valueString:String;
+			
+			if(floatingPointIndex >= 0){
+				valueString = valueStringRaw.substr(0, floatingPointIndex + 3);
 			} else {
-				targetX = X;
+				valueString = valueStringRaw;
 			}
-			//Math.min(xCurDel, xCurDel - xLowestDel) * xRatio +xZeroPos; 
-			var targetY:Float = yZeroPos - Math.min(yCurDel, yCurDel - yLowestDel) * yRatio;
-			graphics.beginFill(0, 1);
-			graphics.drawRect(targetX - 4, targetY-0.5, 4, 1);
-			graphics.endFill();
-
-			if(this.showGridY) {
-				graphics.beginFill(0, 0.1);
-				graphics.drawRect(X, targetY-0.5, WIDTH, 1);
-				graphics.endFill();
-			}
-			if(yCurDel != 0 || !this.lineAtZero){
-				var valueStringRaw:String = Std.string(yCurDel);
-				var floatingPointIndex:Int = valueStringRaw.indexOf(".");
-				var valueString:String;
-				if(floatingPointIndex >= 0){
-					valueString = valueStringRaw.substr(0, floatingPointIndex + 3);
-				} else {
-					valueString = valueStringRaw;
-				}
-
-				var valueField:TextField = new TextField();
-				valueField.selectable = false;
-				valueField.text = valueString;
-				valueField.autoSize = TextFieldAutoSize.LEFT;
-				valueField.x = targetX - valueField.width-5;// valueField.width / 2;
-				valueField.y = targetY - valueField.height / 2;
-				addChild(valueField);
-			}
-			yCurDel += this.yDel;
+			
+			var valueField:TextField = new TextField();
+			valueField.selectable = false;
+			valueField.text = valueString;
+			valueField.autoSize = TextFieldAutoSize.LEFT;
+			valueField.x = targetX - valueField.width - 5;
+			valueField.y = targetY - valueField.height / 2;
+			addChild(valueField);
 		}
+		
 		graphics.endFill();
+		
+		graphics.lineStyle(gridGraphics.axislines.thickness, gridGraphics.axislines.color, gridGraphics.axislines.alpha);
+		
+		graphics.moveTo(xZeroPos + lineX * this.ratio.x, Y);
+		graphics.lineTo(xZeroPos + lineX * this.ratio.x, Y + HEIGHT);
 
-		graphics.lineStyle(1, 0);
-		if(this.lineAtZero){
-			graphics.moveTo(xZeroPos, Y);
-			graphics.lineTo(xZeroPos, Y + HEIGHT);
-
-			graphics.moveTo(X,yZeroPos);
-			graphics.lineTo(X + WIDTH, yZeroPos);
-		} else {
-			graphics.moveTo(X, Y);
-			graphics.lineTo(X, Y + HEIGHT);
-
-			graphics.moveTo(X, Y+HEIGHT);
-			graphics.lineTo(X + WIDTH, Y+HEIGHT);
-		}
+		graphics.moveTo(X, yZeroPos - lineY * this.ratio.y);
+		graphics.lineTo(X + WIDTH, yZeroPos - lineY * this.ratio.y);
+		
+		// Draw borders
+		graphics.lineStyle(gridGraphics.borders.thickness, gridGraphics.borders.color, gridGraphics.borders.alpha);
+		graphics.drawRect(X, Y, WIDTH, HEIGHT);
 		graphics.lineStyle();
 	}
 
@@ -323,4 +342,61 @@ class Grid extends Chart {
 		
 		return new Point(x, y);
 	}
+	
+	private function calculateTicks():Void {
+		ticksX = [0];
+		ticksY = [0];
+	}
+	
+	public function distributeTicks(numTicks:Int, axis:Axis):Void {
+		var distance:Float;
+		var min:Float, max:Float;
+		switch(axis) {
+			case Axis.x:
+				distance = (this.xTop - this.xBottom) / (numTicks-1);
+			case Axis.y:
+				distance = (this.yTop - this.yBottom) / (numTicks-1);
+			case Axis.z:
+				return;
+		}
+		distributeTicksDist(distance, axis);
+	}
+	
+	public function distributeTicksDist(distance:Float, axis:Axis, ?first:Float):Void {
+		var max:Float;
+		
+		switch(axis) {
+			case Axis.x:
+				max = this.xTop;
+				if (first == null) {
+					first = this.xBottom;
+				}
+			case Axis.y:
+				max = this.yTop;	
+				if (first == null) {
+					first = this.yBottom;
+				}
+			case Axis.z:
+				return;
+		}
+		
+		var ticks:Array<Float> = [];
+		var current = first;
+		
+		while (Std.int(current) <= max) {
+			trace(current, max);
+			ticks.push(current);
+			current += distance;
+		}
+		
+		switch(axis) {
+			case Axis.x:
+				ticksX = ticks;
+			case Axis.y:
+				ticksY = ticks;
+			case Axis.z:
+				return;
+		}
+	}
+	
 }
